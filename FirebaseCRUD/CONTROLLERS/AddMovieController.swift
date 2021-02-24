cf79f7562acfa90074fd10c71bca37245539fbd8
fred milloh <fred.milloh@hotmail.com>
2021-02-24 19:00:57 +0100

    add blur

--- FirebaseCRUD/CONTROLLERS/AddMovieController.swift
+++ FirebaseCRUD/CONTROLLERS/AddMovieController.swift
@@ -8,15 +8,21 @@
 import UIKit
 
 class AddMovieController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var directorTF: UITextField!
+    @IBOutlet weak var pitchTV: UITextView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
     }
     
     @IBAction func dismissPopup(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addMovie(_ sender: UIButton) {
     }
     
     
