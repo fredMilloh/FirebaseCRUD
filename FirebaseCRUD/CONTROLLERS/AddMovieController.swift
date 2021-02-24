1167e224a97896019c08e0a49f50a3ea39dcb56d
fred milloh <fred.milloh@hotmail.com>
2021-02-24 17:16:57 +0100

    AddMovieController UI

---
+++ FirebaseCRUD/CONTROLLERS/AddMovieController.swift
@@ -0,0 +1,24 @@
+//
+//  AddMovieController.swift
+//  FirebaseCRUD
+//
+//  Created by fred on 24/02/2021.
+//
+
+import UIKit
+
+class AddMovieController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func dismissPopup(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
